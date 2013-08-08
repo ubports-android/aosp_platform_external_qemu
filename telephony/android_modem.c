5f698cc392964fe4525c51f583caaaf64a3134b8
Hsin-Yi Tsai <htsai@mozilla.com>
2013-08-08 18:25:45 +0800

    Bug 845174 - B2G emulator: support RIL_UNSOL_CALL_RING. r=vicamo

--- telephony/android_modem.c
+++ telephony/android_modem.c
@@ -1060,9 +1060,7 @@ amodem_send_stk_unsol_proactive_command( AModem  modem, const char* stkCmdPdu )
 static void
 amodem_send_calls_update( AModem  modem )
 {
-   /* despite its name, this really tells the system that the call
-    * state has changed */
-    amodem_unsol( modem, "RING\r" );
+    amodem_unsol( modem, "CALL STATE CHANGED\r" );
 }
 
 
@@ -1090,7 +1088,7 @@ amodem_add_inbound_call( AModem  modem, const char*  number )
     memcpy( call->number, number, len );
     call->number[len] = 0;
 
-    amodem_send_calls_update( modem );
+    amodem_unsol( modem, "RING\r");
     return 0;
 }
 
@@ -1165,7 +1163,7 @@ int amodem_remote_call_busy( AModem  modem, const char*  number )
         return -1;
 
     amodem_free_call(modem, vcall, CALL_FAIL_BUSY);
-    amodem_send_calls_update(modem);
+    amodem_unsol( modem, "NO CARRIER\r");
     return 0;
 }
 
@@ -1179,7 +1177,7 @@ amodem_disconnect_call( AModem  modem, const char*  number )
         return -1;
 
     amodem_free_call( modem, vcall, CALL_FAIL_NORMAL );
-    amodem_send_calls_update(modem);
+    amodem_unsol( modem, "NO CARRIER\r");
     return 0;
 }
 
@@ -1196,7 +1194,7 @@ amodem_clear_call( AModem modem )
         AVoiceCall call = (AVoiceCall) &vcall->call;
         amodem_free_call( modem, call, CALL_FAIL_NORMAL );
     }
-    amodem_send_calls_update(modem);
+    amodem_unsol( modem, "NO CARRIER\r");
 
     return 0;
 }
@@ -2356,7 +2354,7 @@ remote_voice_call_event( void*  _vcall, int  success )
     if (!success) {
         /* aargh, the remote emulator probably quitted at that point */
         amodem_free_call(modem, vcall, CALL_FAIL_NORMAL);
-        amodem_send_calls_update(modem);
+        amodem_unsol( modem, "NO CARRIER\r");
     }
 }
 
