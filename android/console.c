19397fdd7d0e7f19a91aebc487ee8c7fb4427edc
Thomas Zimmermann <tdz@users.sourceforge.net>
2014-03-31 17:34:55 +0200

    Bug 995202: Pass NCI payload size to callback functions
    The NCI payload buffer is limited to 256 bytes, so it can easily
    overflow. The callback function for creating NCI payload now
    receives a parameter that contains the remaining buffer size.
    
    Signed-off-by: Thomas Zimmermann <tdz@users.sourceforge.net>

--- android/console.c
+++ android/console.c
@@ -3366,7 +3366,7 @@ struct nfc_ntf_param {
 
 static ssize_t
 nfc_rf_discovery_ntf_cb(void* data,
-                        struct nfc_device* nfc,
+                        struct nfc_device* nfc, size_t maxlen,
                         union nci_packet* ntf)
 {
     ssize_t res;
@@ -3381,7 +3381,7 @@ nfc_rf_discovery_ntf_cb(void* data,
 
 static ssize_t
 nfc_rf_intf_activated_ntf_cb(void* data,
-                             struct nfc_device* nfc,
+                             struct nfc_device* nfc, size_t maxlen,
                              union nci_packet* ntf)
 {
     size_t res;
@@ -3487,7 +3487,7 @@ struct nfc_dta_write_param {
 };
 
 static ssize_t
-nfc_dta_write_cb(void* data, struct nfc_device* nfc,
+nfc_dta_write_cb(void* data, struct nfc_device* nfc, size_t maxlen,
                  union nci_packet* packet)
 {
   const struct nfc_dta_write_param* param;
